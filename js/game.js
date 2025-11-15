afb5005c4592c29e08388d40eadd066b3e5f442e
Fu1rkan <furkanyigit2000@gmail.com>
2025-11-15 14:54:21 +0100

    character img draws in Canvas

--- js/game.js
+++ js/game.js
@@ -1,7 +1,12 @@
-let canvass;
+let canvas;
+let ctx;
 let character = new Image();
 
-function ini(){
-    canvass = document.getElementById('canvas');
+function init(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
     character.src = './img/2_character_pepe/2_walk/W-21.png';
+    
+    ctx.drawImage(character, 20, 20, 50, 150);
 }
