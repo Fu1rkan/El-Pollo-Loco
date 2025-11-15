5474d1dcc6eba9890eeffd15b0d32fb5430e5c29
Fu1rkan <furkanyigit2000@gmail.com>
2025-11-15 21:10:07 +0100

    Caracter and chicken drawing auto

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,6 @@
 class Chicken extends MovableObject {
-    
+    x = 50;
+    y = 50;
 
     constructor(){
         super().loadImg('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,9 @@
 class MovableObject {
-    x = 120;
-    y = 400;
+    x = 0;
+    y = 0;
     img;
+    height = 50;
+    width = 50;
 
     loadImg(path){
         this.img = new Image();
