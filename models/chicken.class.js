2f38c47cd1a321adc2c6b69fe86b7810b769dcdb
Furkan Yigit <furkanyigit2000@gmail.com>
2025-11-16 22:26:05 +0100

    added smooth animation for the chickens

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,10 +1,18 @@
 class Chicken extends MovableObject {
-    
-    constructor(w, h){
+
+    constructor(w, h) {
         super().loadImg('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random() * 1140;
         this.y = 425 - h;
         this.width = w;
         this.height = h;
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.3;
+        }, 1000 / 60)
     }
 }
