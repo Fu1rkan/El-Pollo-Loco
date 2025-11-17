9b102a69f931f3e6475a8889608369d479ef47f7
Furkan Yigit <furkanyigit2000@gmail.com>
2025-11-17 20:52:10 +0100

    I created a level folder and made the structure of the world more organized.

---
+++ level/level1.js
@@ -0,0 +1,37 @@
+const level1 = new Level(
+    [
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80),
+        new Chicken(80, 80)
+    ],
+    [
+        new Clouds('img/5_background/layers/4_clouds/1.png', 720, 480, 0, 0),
+        new Clouds('img/5_background/layers/4_clouds/2.png', 720, 480, 720, 0),
+        new Clouds('img/5_background/layers/4_clouds/1.png', 720, 480, 1440, 0),
+        new Clouds('img/5_background/layers/4_clouds/2.png', 720, 480, 2160, 0)
+    ],
+    [
+        new Background('img/5_background/layers/air.png', 720, 480, 0, 0),
+        new Background('img/5_background/layers/3_third_layer/1.png', 720, 480, 0, 0),
+        new Background('img/5_background/layers/2_second_layer/1.png', 720, 480, 0, 0),
+        new Background('img/5_background/layers/1_first_layer/1.png', 720, 480, 0, 0),
+        new Background('img/5_background/layers/air.png', 720, 480, 720, 0),
+        new Background('img/5_background/layers/3_third_layer/2.png', 720, 480, 720, 0),
+        new Background('img/5_background/layers/2_second_layer/2.png', 720, 480, 720, 0),
+        new Background('img/5_background/layers/1_first_layer/2.png', 720, 480, 720, 0),
+        new Background('img/5_background/layers/air.png', 720, 480, 1440, 0),
+        new Background('img/5_background/layers/3_third_layer/1.png', 720, 480, 1440, 0),
+        new Background('img/5_background/layers/2_second_layer/1.png', 720, 480, 1440, 0),
+        new Background('img/5_background/layers/1_first_layer/1.png', 720, 480, 1440, 0),
+        new Background('img/5_background/layers/air.png', 720, 480, 2160, 0),
+        new Background('img/5_background/layers/3_third_layer/2.png', 720, 480, 2160, 0),
+        new Background('img/5_background/layers/2_second_layer/2.png', 720, 480, 2160, 0),
+        new Background('img/5_background/layers/1_first_layer/2.png', 720, 480, 2160, 0),
+    ]
+)
--- models/world.class.js
+++ models/world.class.js
@@ -1,44 +1,9 @@
 class World {
     character = new Character(25, 225, 100, 200);
 
-    enemies = [
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80),
-        new Chicken(80, 80)
-    ];
-
-    clouds = [
-        new Clouds('img/5_background/layers/4_clouds/1.png', 720, 480, 0, 0),
-        new Clouds('img/5_background/layers/4_clouds/2.png', 720, 480, 720, 0),
-        new Clouds('img/5_background/layers/4_clouds/1.png', 720, 480, 1440, 0),
-        new Clouds('img/5_background/layers/4_clouds/2.png', 720, 480, 2160, 0)
-    ];
-
-    background = [
-        new Background('img/5_background/layers/air.png', 720, 480, 0, 0),
-        new Background('img/5_background/layers/3_third_layer/1.png', 720, 480, 0, 0),
-        new Background('img/5_background/layers/2_second_layer/1.png', 720, 480, 0, 0),
-        new Background('img/5_background/layers/1_first_layer/1.png', 720, 480, 0, 0),
-        new Background('img/5_background/layers/air.png', 720, 480, 720, 0),
-        new Background('img/5_background/layers/3_third_layer/2.png', 720, 480, 720, 0),
-        new Background('img/5_background/layers/2_second_layer/2.png', 720, 480, 720, 0),
-        new Background('img/5_background/layers/1_first_layer/2.png', 720, 480, 720, 0),
-        new Background('img/5_background/layers/air.png', 720, 480, 1440, 0),
-        new Background('img/5_background/layers/3_third_layer/1.png', 720, 480, 1440, 0),
-        new Background('img/5_background/layers/2_second_layer/1.png', 720, 480, 1440, 0),
-        new Background('img/5_background/layers/1_first_layer/1.png', 720, 480, 1440, 0),
-        new Background('img/5_background/layers/air.png', 720, 480, 2160, 0),
-        new Background('img/5_background/layers/3_third_layer/2.png', 720, 480, 2160, 0),
-        new Background('img/5_background/layers/2_second_layer/2.png', 720, 480, 2160, 0),
-        new Background('img/5_background/layers/1_first_layer/2.png', 720, 480, 2160, 0),
-    ];
-
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    background = level1.background;
     canvas;
     ctx;
     keyboard;
