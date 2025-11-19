5866dafbcd550a29ef35b9688a7e584793985d95
Furkan Yigit <furkanyigit2000@gmail.com>
2025-11-20 00:34:14 +0100

    throwing buttom animation added

--- models/throwable_object.class.js
+++ models/throwable_object.class.js
@@ -1,4 +1,21 @@
 class ThrowableObject extends MovableObject {
+
+    IMAGES_THROW_BOTTLE = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ]
+
+    IMAGES_BOTTLE_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ]
+
     constructor(x, y) {
         super();
         this.x = x;
@@ -6,14 +23,23 @@ class ThrowableObject extends MovableObject {
         this.width = 80;
         this.height = 100;
         this.loadImg('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_THROW_BOTTLE);
+        this.loadImages(this.IMAGES_BOTTLE_SPLASH);
         this.trow();
     }
-
+    
     trow() {
         this.speedY = 30;
         this.applyGravity();
+        this.animate();
         setInterval(() => {
             this.x += 10
         }, 25)
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_THROW_BOTTLE);
+        }, 75);
     }
 }
