7a2ffa4d62956db1500f1c9d483b512cc2a9eb07
Furkan Yigit <furkanyigit2000@gmail.com>
2026-02-06 18:12:45 +0100

    added attack, alert, death, hurt and walk imgs to the endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,4 +1,10 @@
 class Endboss extends MovableObject {
+    IMAGES_WALK = [
+        'img/4_enemie_boss_chicken/1_WALK/G1.png',
+        'img/4_enemie_boss_chicken/1_WALK/G2.png',
+        'img/4_enemie_boss_chicken/1_WALK/G3.png',
+        'img/4_enemie_boss_chicken/1_WALK/G4.png'
+    ]
 
     IMAGES_STANDING = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
@@ -8,13 +14,40 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G9.png',
         'img/4_enemie_boss_chicken/2_alert/G10.png',
         'img/4_enemie_boss_chicken/2_alert/G11.png',
-        'img/4_enemie_boss_chicken/2_alert/G12.png'
+        'img/4_enemie_boss_chicken/2_alert/G12.png',
     ];
+
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png'
+    ]
+    
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png'
+    ]
+    
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png'
+    ]
 
     constructor() {
         super();
         this.loadImg(this.IMAGES_STANDING[0]);
         this.loadImages(this.IMAGES_STANDING);
+        this.loadImages(this.IMAGES_WALK);
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 3880;
         this.y = 140;
         this.width = 300;
@@ -27,11 +60,11 @@ class Endboss extends MovableObject {
         let intervalId = setInterval(() => {
             if (isRunning) {
                 
-                if (2 + 2 == 4) {
-                    
-                    
+                if (world.character.x >= 3600) {
+                    this.playAnimation(this.IMAGES_ATTACK);
+                }else{
+                    this.playAnimation(this.IMAGES_STANDING);
                 }
-                this.playAnimation(this.IMAGES_STANDING);
             }
         }, 500);
         DrawableObject.intervalArr.push(intervalId);
